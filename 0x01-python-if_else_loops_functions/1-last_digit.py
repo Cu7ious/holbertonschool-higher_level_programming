3b7084a6461b1d5c53381962d5d1cc223dc064f4
Sergii Garkusha <328@holbertonschool.com>
2018-04-10 16:24:04 -0700

    Replaced is with ==

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ last_digit = number % 10
 if last_digit > 5:
     print("Last digit of {} is {} and is greater than 5"
           .format(number, last_digit))
-elif last_digit is 0:
+elif last_digit == 0:
     print("Last digit of {} is 0 and is 0"
           .format(number))
 elif last_digit < 6 and last_digit != 0:
