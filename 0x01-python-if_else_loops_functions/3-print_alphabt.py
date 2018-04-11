02874a5e4c8ffd2e2316eae28c23f7e95910beb1
Sergii Garkusha <328@holbertonschool.com>
2018-04-10 21:25:54 -0700

    Rewrited with numbers

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(97, 122):
-    if chr(i) == 'e' or chr(i) == 'q':
+    if i is 101 or i is 113:
         continue
     else:
         print("{:s}".format(chr(i)), end="")
