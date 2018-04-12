70493f88404a00c975b6ffd95256ff69f4dbed8f
Sergii Garkusha <328@holbertonschool.com>
2018-04-12 16:14:42 -0700

    Fixed typo: unused vars

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -7,7 +7,7 @@ def print_add():
     a = 1
     b = 2
 
-    print("{:d} + {:d} = {:d}".format(a, b, add(1, 2)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 if __name__ == "__main__":
     print_add()
