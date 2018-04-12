ad94bf4351ff33ce0ba88e3bcb88c93c955cd413
Sergii Garkusha <328@holbertonschool.com>
2018-04-12 16:12:17 -0700

    Rewrited with from syntax

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    add = __import__("add_0").add
+
+def print_add():
+    from add_0 import add
 
     a = 1
     b = 2
 
     print("{:d} + {:d} = {:d}".format(a, b, add(1, 2)))
+
+if __name__ == "__main__":
+    print_add()
