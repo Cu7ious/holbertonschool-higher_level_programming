943df704211f381914b261d85b34be6242fbc864
Sergii Garkusha <328@holbertonschool.com>
2018-04-12 16:23:05 -0700

    Added files for task #1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def print_calcs():
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+if __name__ == "main":
+    print_calcs()
