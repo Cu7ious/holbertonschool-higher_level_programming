e83ba9323d8690a6bb6f61deb71ecb236606c85b
Cu7ious <328@holbertonschool.com>
2018-04-18 04:47:51 +0000

    Changed solution to a join(), task #6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,12 +4,5 @@
 def print_matrix_integer(matrix=[[]]):
     """Prints a matrix of integers."""
 
-    if len(matrix[0]) is 0:
-        print("");
-
     for row in matrix:
-        for el in row:
-            if row[len(row) - 1] is el:
-                print("{:d}".format(el))
-            else:
-                print("{:d} ".format(el), end="")
+        print(" ".join(["{:d}".format(el) for el in row]))
