723e7ac5ccdc42f6b37722e02dd93a9ffb167c56
Cu7ious <328@holbertonschool.com>
2018-04-18 05:06:12 +0000

    Added if sentence is empty confition, task #8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@
 
 def multiple_returns(sentence):
     """Returns a tuple with the length of a string and its first character."""
-
-    return (len(sentence), sentence[0])
+    char = sentence[0] if sentence else None
+    return (len(sentence), char)
