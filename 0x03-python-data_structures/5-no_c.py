f20f756640fda3077fdcc18a1e4ea0cc673ebaa2
Cu7ious <328@holbertonschool.com>
2018-04-18 04:05:18 +0000

    Added solution for the task #5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,15 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
+
+def no_c(my_string):
+    """Removes all characters c and C from a string."""
+
+    result = ""
+
+    for char in my_string:
+        if char is "c" or char is "C":
+            continue
+
+        result += char
+
+    return result
