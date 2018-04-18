417780032ff23102ddc904bd2f4c0e6cadac7af6
Cu7ious <328@holbertonschool.com>
2018-04-18 04:43:09 +0000

    Added solution for the task #6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,15 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
+
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers."""
+
+    if len(matrix[0]) is 0:
+        print("");
+
+    for row in matrix:
+        for el in row:
+            if row[len(row) - 1] is el:
+                print("{:d}".format(el))
+            else:
+                print("{:d} ".format(el), end="")
