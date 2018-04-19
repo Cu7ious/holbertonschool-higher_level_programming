02756aad33e3efa6907fe997eb32f4dc735a39bb
Cu7ious <328@holbertonschool.com>
2018-04-19 21:57:58 +0000

    Fixed pep8

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,10 @@
 
 
 def square_matrix_simple(matrix=[]):
-	"""Computes the square value of all integers of a matrix."""
-	result = []
+    """Computes the square value of all integers of a matrix."""
+    result = []
 
-	for row in matrix:
-		result.append(list(map(lambda x: x**2, row)))
+    for row in matrix:
+        result.append(list(map(lambda x: x**2, row)))
 
-	return result
+    return result
