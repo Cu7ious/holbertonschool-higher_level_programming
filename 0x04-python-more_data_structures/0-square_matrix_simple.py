3e3f6a03387d30f1b6fdae78df76eab22930a4e1
Cu7ious <328@holbertonschool.com>
2018-04-19 21:44:09 +0000

    Added solution for the task #0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+	"""Computes the square value of all integers of a matrix."""
+	result = []
+
+	for row in matrix:
+		result.append(list(map(lambda x: x**2, row)))
+
+	return result
