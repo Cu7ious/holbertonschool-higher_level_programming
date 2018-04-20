652f617a686b984a09b4c991f2e4f5d6994f8b13
Cu7ious <328@holbertonschool.com>
2018-04-20 03:17:08 +0000

    Added check for the case when the string is empty

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,27 +13,27 @@ def get_char_val(string, idx):
 def roman_to_int(roman_string):
     """Converts a Roman number to an integer."""
 
-    if (isinstance(roman_string, str) is False):
+    if (roman_string and isinstance(roman_string, str) is True):
+        result = 0
+        next_c = curr_c = 0
+        index = len(roman_string) - 1
+
+        while(index >= 0):
+            c = index
+
+            curr_c = get_char_val(roman_string, c)
+            next_c = get_char_val(roman_string, c - 1)
+
+            result += curr_c
+
+            if next_c < curr_c:
+                result -= next_c
+
+                if index - 1 is 0:
+                    break
+
+            index -= 1
+
+        return result
+    else:
         return 0
-
-    result = 0
-    next_c = curr_c = 0
-    index = len(roman_string) - 1
-
-    while(index >= 0):
-        c = index
-
-        curr_c = get_char_val(roman_string, c)
-        next_c = get_char_val(roman_string, c - 1)
-
-        result += curr_c
-
-        if next_c < curr_c:
-            result -= next_c
-
-            if index - 1 is 0:
-                break
-
-        index -= 1
-
-    return result
