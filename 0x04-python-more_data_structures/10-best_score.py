6b68090137a1c7fd2c288d7c03771aa3a0b22e9a
Cu7ious <328@holbertonschool.com>
2018-04-20 02:46:53 +0000

    Simplification

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@
 def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
 
-    if isinstance(a_dictionary, dict) is False:
-        return None
+    if a_dictionary:
+        return sorted(a_dictionary, key=a_dictionary.get, reverse=True)[0]
 
-    return sorted(a_dictionary, key=a_dictionary.get, reverse=True)[0]
+    return None
