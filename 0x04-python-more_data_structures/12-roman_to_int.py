e829da7a8c6bb467fccea4225356812e264de391
Cu7ious <328@holbertonschool.com>
2018-04-20 03:27:36 +0000

    Revrited using simplified solution: started form the head of the string, subtracting the value of the number if it should be enterpret as a lower one

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,38 +1,22 @@
 #!/usr/bin/python3
 
 
-def get_char_val(string, idx):
-    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-
-    if (idx < 0):
-        return 0
-
-    return roman[string[idx]]
-
-
 def roman_to_int(roman_string):
     """Converts a Roman number to an integer."""
 
     if (roman_string and isinstance(roman_string, str) is True):
+        roman = {
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
+        }
+        prev = 0
         result = 0
-        next_c = curr_c = 0
-        index = len(roman_string) - 1
 
-        while(index >= 0):
-            c = index
+        for c in roman_string:
+            if prev < roman[c]:
+                result -= prev * 2
 
-            curr_c = get_char_val(roman_string, c)
-            next_c = get_char_val(roman_string, c - 1)
-
-            result += curr_c
-
-            if next_c < curr_c:
-                result -= next_c
-
-                if index - 1 is 0:
-                    break
-
-            index -= 1
+            prev = roman[c]
+            result += roman[c]
 
         return result
     else:
