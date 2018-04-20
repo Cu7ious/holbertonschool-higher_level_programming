cdf20e174f82ca262fdbbac7bbc398531abc3ef6
Cu7ious <328@holbertonschool.com>
2018-04-20 01:03:43 +0000

    Added the simple test for the task #10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+
+    if (a_dictionary is None):
+        return None
+
+    key = ""
+    score = 0
+
+    for k, v in a_dictionary.items():
+        if score <= v:
+            key = k
+            score = v
+
+    return key
