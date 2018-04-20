2ab3407998607be459a6174bb67f0193d4d53271
Cu7ious <328@holbertonschool.com>
2018-04-20 03:57:13 +0000

    Added the solution for the advanced task #15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+
+    if (value in a_dictionary.values() is False):
+        return a_dictionary
+
+    keys_to_del = []
+
+    for item in a_dictionary.items():
+        if item[1] == value:
+            keys_to_del.append(item[0])
+
+    for key in keys_to_del:
+        del a_dictionary[key]
+
+    return a_dictionary
