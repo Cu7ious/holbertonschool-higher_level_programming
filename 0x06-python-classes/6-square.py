c9c14e3da708dbcbc7bc221c796b8f125f123a25
Cu7ious <328@holbertonschool.com>
2018-05-02 02:59:10 +0000

    Added formatted strings

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,19 +30,6 @@ class Square:
         """Returns the size of the square"""
         return self.__size ** 2
 
-    def my_print(self):
-        """Prints the square with the character #, padded with spaces
-        into the stdout."""
-        if self.size is 0:
-            print()
-        else:
-            if self.position[1] > 0:
-                for j in range(0, self.position[1]):
-                    print(" " * (self.position[0] + self.size))
-
-            for i in range(0, self.size):
-                print(" " * self.position[0] + "#" * self.size)
-
     @property
     def position(self):
         """The __position getter"""
@@ -61,3 +48,15 @@ class Square:
             raise TypeError("Position must be a tuple of 2 positive integers")
 
         self.__position = value
+
+    def my_print(self):
+        """Prints the square with the character #, padded with spaces
+        into the stdout."""
+        if self.size is 0:
+            print()
+        else:
+            for j in range(0, self.position[1]):
+                print("{:s}".format(" " * (self.position[0] + self.size)))
+
+            for i in range(0, self.size):
+                print("{:s}".format(" " * self.position[0] + "#" * self.size))
