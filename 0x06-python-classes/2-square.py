32976b6d7ca6e2e0c6276819d1c9dd3e495849f2
Cu7ious <328@holbertonschool.com>
2018-05-02 01:11:47 +0000

    Added the solution for the task #2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self, num=None):
+
+        if num:
+            if isinstance(num, int) is not True:
+                raise TypeError("size must be an integer")
+                return
+
+            if num < 0:
+                raise ValueError("size must be >= 0")
+                return
+
+            self.__size = num
