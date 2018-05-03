7e94c0c2ba4fa008fc56216a1c0f1a4f045f8778
Cu7ious <328@holbertonschool.com>
2018-05-03 14:59:59 +0000

    Implemented the function for the task #0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""This module creates the function that adds 2 integers"""
 
 
 def add_integer(a, b=98):
-    pass
+    """Adds 2 integers."""
+
+    if not isinstance(a, int) or not isinstance(a, float):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, int) or not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
