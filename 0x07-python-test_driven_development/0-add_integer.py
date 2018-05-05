bdf98b5f4790b6d2a30597bb0103446003eeecec
Cu7ious <328@holbertonschool.com>
2018-05-05 01:40:22 +0000

    Corrected logic of the type check

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""This module creates the function that adds 2 integers"""
+"""
+    This module creates the function that adds 2 integers
+
+    add_integer(2, 2) # 4
+"""
 
 
 def add_integer(a, b=98):
-    """Adds 2 integers."""
+    """
+        Adds 2 integers.
+    """
 
-    if not isinstance(a, int) or not isinstance(a, float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, int) or not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
 
     if isinstance(a, float):
