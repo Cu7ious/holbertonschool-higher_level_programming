0eee9b6883ee2cf8826d08fde2a6437b1e930e05
Cu7ious <328@holbertonschool.com>
2018-05-09 03:23:27 +0000

    Beautified docstrings & added new test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
@@ -9,29 +8,29 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
+    def test_strings_instead_of_ints(self):
+        self.assertEqual(max_integer(["K", "r", "a", "f", "t", "w"]), "erk")
+
+    def test_strings_and_int(self):
+        self.assertRaises(TypeError, max_integer, ["a", "b", 1])
+
     def test_basic(self):
         self.assertEqual(max_integer([1, 2, 3, 4, 5, 6]), 6)
 
-    def test_basic1(self):
+    def test_basic_one_element(self):
         self.assertEqual(max_integer([1]), 1)
 
-    def test_basic2(self):
+    def test_basic_two_els(self):
         self.assertEqual(max_integer([1,2]), 2)
 
-    def test_basic3(self):
+    def test_basic_all_negatives(self):
         self.assertEqual(max_integer([-2, -1]), -1)
 
-    def test_basic4(self):
+    def test_basic_negative_zero_zero(self):
         self.assertEqual(max_integer([-1, 0]), 0)
 
-    def test_weird(self):
-        self.assertEqual(max_integer(["Hel", "Wor", "str", "a"]), "str")
-
-    def test_weird1(self):
-        self.assertEqual(max_integer(["a", "b"]), "b")
-
-    def test_weird2(self):
-        self.assertRaises(TypeError, max_integer, ["a", "b", 1])
+    def test_basic_(self):
+        self.assertEqual(max_integer([6, 1, 2, 3]), 6)
 
 if __name__ == '__main__':
     unittest.main()
