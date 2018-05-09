2a1a5764194a53e4ee00ef12aeffd93fc3cfdd57
Cu7ious <328@holbertonschool.com>
2018-05-09 07:04:18 +0000

    Bath 0.1

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    This module defines the function that copies a list
+"""
+
+
+def copy_list(l):
+    """
+        copy_list - copies a list and returns it
+        @: a list to copy
+        Return: the copy of a list l
+    """
+    return l[:]
