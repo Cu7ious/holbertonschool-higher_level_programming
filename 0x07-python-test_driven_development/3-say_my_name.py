b0eae3f8804cdc5ae072025a3f350001cabfd167
Cu7ious <328@holbertonschool.com>
2018-05-09 02:56:37 +0000

    Fixed the hashbang

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,21 +1,21 @@
-##!/usr/bin/python3
+#!/usr/bin/python3
 """
-This module contains `say_my_name` function.
-Accepts: two strings, first_name & last_name
-It will print out a string with the two input strings
-Checks: type first_name & last_name is string
+    This module contains `say_my_name` function.
+    Accepts: two strings, first_name & last_name
+    It will print out a string with the two input strings
+    Checks: type first_name & last_name is string
 """
 
 
 def say_my_name(first_name, last_name=""):
-        """
-        say_my_name: prints a message with the input names
-        """
+    """
+    say_my_name: prints a message with the input names
+    """
 
-        if not isinstance(first_name, str):
-            raise TypeError("first_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
 
-        if not isinstance(last_name, str):
-            raise TypeError("last_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
 
-        print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
