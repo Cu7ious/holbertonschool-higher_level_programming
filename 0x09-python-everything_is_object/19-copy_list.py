399980d33add084020f347ab75c43c7ac1a8caae
Cu7ious <328@holbertonschool.com>
2018-05-09 16:38:24 +0000

    Got rid of the docstrings (task requirement)

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-"""
-    This module defines the function that copies a list
-"""
-
-
 def copy_list(l):
-    """
-        copy_list - copies a list and returns it
-        @: a list to copy
-        Return: the copy of a list l
-    """
     return l[:]
