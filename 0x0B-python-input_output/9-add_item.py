8871ee92c08816223172a9b473ddd20b3c4fa605
Cu7ious <328@holbertonschool.com>
2018-05-17 03:22:46 +0000

    Removed the unused  module

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 from sys import argv
-import json
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 """
