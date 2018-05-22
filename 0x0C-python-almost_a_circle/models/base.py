9e9478e3a11840b9dd8a7d67687b056c556604d7
Cu7ious <328@holbertonschool.com>
2018-05-22 21:17:32 +0000

    Added from_json_string static method and the create class method to the Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,7 +23,11 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Returns the JSON string representation of the instance"""
+        """ Returns the JSON string representation of the instance
+
+        Args:
+            list_dictionaries: the list of dictionaries
+        """
         if list_dictionaries is None or len(list_dictionaries) is 0:
             return "[]"
 
@@ -31,7 +35,12 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """ Writes the JSON string representation of list_objs to a file"""
+        """ Writes the JSON string representation of list_objs to a file
+
+        Args:
+            cls: the reference to the class
+            list_objs: the list of objects to save
+        """
         content = []
 
         if list_objs is not None:
@@ -41,3 +50,31 @@ class Base:
         with open(cls.__name__ + ".json", 'w') as a_file:
             a_file.write(cls.to_json_string(content))
 
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns the list of the JSON string representation
+
+        Args:
+            json_string: string representing a list of dictionaries
+        """
+        if json_string is None:
+            return "[]"
+
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set
+
+        Args:
+            cls: the reference to the class
+            dictionary: the double pointer to a dictionary
+        """
+        if cls.__name__ is "Rectangle":
+            temp = cls(1, 1)
+        elif cls.__name__ is "Square":
+            temp = cls(1)
+
+        temp.update(**dictionary)
+
+        return temp
