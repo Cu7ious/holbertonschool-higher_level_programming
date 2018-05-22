a9f6e23771de6477adb35b10ce3b83738159fcc6
Cu7ious <328@holbertonschool.com>
2018-05-22 03:42:16 +0000

    Added to_json_string() method to the Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """ Module contains the `Base` class
 """
 
@@ -19,3 +20,10 @@ class Base:
             id = self.__nb_objects
 
         self.id = id
+
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of the instance"""
+        if list_dictionaries is None or len(list_dictionaries) is 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
