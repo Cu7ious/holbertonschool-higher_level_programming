8d21ec9069c6b2051a37bb8b0ff3dd639a26f424
Cu7ious <328@holbertonschool.com>
2018-05-23 06:45:32 +0000

    Updated tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -123,16 +123,103 @@ class TestRectangleClass(unittest.TestCase):
         Base._Base__nb_objects = 0
 
     def test_to_json_string(self):
-        """
-        test json string
-        """
+        """ JSON string"""
         temp = Rectangle(1, 1, 1, 1, 1)
         r1 = temp.to_dictionary()
         r2 = temp.to_json_string(r1)
         self.assertEqual(r2, json.dumps(r1))
         Base._Base__nb_objects = 0
 
+    def test_json_string_to_file(self):
+        """ From json file"""
+        temp1 = Rectangle(1, 1, 1, 1, 1)
+        temp2 = Rectangle(5, 6, 7, 8, 9)
+        Rectangle.save_to_file([temp1, temp2])
+        with open("Rectangle.json") as file:
+            r1 = file.read()
+            r2 = [temp1.to_dictionary(), temp2.to_dictionary()]
+            self.assertEqual(json.dumps(r2), r1)
+        Base._Base__nb_objects = 0
+
+    def test_from_json(self):
+        """ From json"""
+        temp1 = Rectangle(1, 1, 1, 1, 1)
+        temp2 = Rectangle(5, 6, 7, 8, 9)
+        r1 = [temp1.to_dictionary(), temp2.to_dictionary()]
+        self.assertTrue(isinstance(r1, list))
+        r2 = Rectangle.to_json_string(r1)
+        self.assertTrue(isinstance(r2, str))
+        r3 = Rectangle.from_json_string(r2)
+        self.assertTrue(isinstance(r3, list))
+        Base._Base__nb_objects = 0
+
+    def test_create(self):
+        """ Create method"""
+        r1 = Rectangle(1, 1, 1)
+        dict1 = r1.to_dictionary()
+        r2 = Rectangle.create(**dict1)
+        self.assertFalse(r1 == r2)
+        self.assertFalse(r1 is r2)
+        Base._Base__nb_objects = 0
+
+    def test_load_from_file(self):
+        """ Loading from json file"""
+        r1 = Rectangle(1, 1, 1, 1, 1)
+        r2 = Rectangle(5, 6, 7, 8, 2)
+        lists = [r1, r2]
+        Rectangle.save_to_file(lists)
+
+        out = Rectangle.load_from_file()
+        self.assertTrue(isinstance(out, list))
+
+        self.assertTrue(isinstance(out[0], Rectangle))
+        self.assertTrue(isinstance(out[1], Rectangle))
+        self.assertEqual(str(r1), str(out[0]))
+        self.assertEqual(str(r2), str(out[1]))
+        Base._Base__nb_objects = 0
+
+    def test_return_empty(self):
+        """ json_to_string returns none"""
+        output = Rectangle.to_json_string(None)
+        self.assertEqual(output, "[]")
+        output = Rectangle.to_json_string([])
+        self.assertEqual(output, "[]")
+
+    def test_save_empty(self):
+        """ Empty list input"""
+        lists = []
+        Rectangle.save_to_file(lists)
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual("[]", f.read())
+
+    def test_save_None(self):
+        """ Input is none"""
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual("[]", f.read())
+
+    def test_load_no_file(self):
+        """ File doesnt exist"""
+        try:
+            Rectangle.save_to_file(None)
+            os.remove("Rectangle.json")
+        except BaseException:
+            pass
+
+        self.assertEqual(Rectangle.load_from_file(), [])
+
+    def test_load_empty_file(self):
+        """ File is empty"""
+        try:
+            Rectangle.save_to_file(None)
+            os.remove("Rectangle.json")
+        except BaseException:
+            pass
+
+        open("Rectangle.json", 'a').close()
+        self.assertEqual(Rectangle.load_from_file(), [])
+
     def test_class_basic_init(self):
-        """ ids"""
+        """ Ids"""
         self.assertEqual(Rectangle(1, 1).id, 1)
         Base._Base__nb_objects = 0
