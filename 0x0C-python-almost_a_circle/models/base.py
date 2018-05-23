e1e40a1c91721ad42fb88eb64ba8e98a5e2f68c9
Cu7ious <328@holbertonschool.com>
2018-05-23 02:18:59 +0000

    Rewrited the load_from_file method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -86,12 +86,14 @@ class Base:
         Args:
             cls: the reference to the class
         """
-        lists = []
         try:
-            with open(cls.__name__ + ".json", "r", encoding="utf-8") as opened:
-                lists = cls.from_json_string(opened.read())
-            for i in range(len(lists)):
-                lists[i] = cls.create(**lists[i])
-            return lists
+            with open(cls.__name__ + ".json", "r") as a_file:
+                content = cls.from_json_string(a_file.read())
+
+                result = []
+                for el in content:
+                    result.append(cls.create(**el))
+
+                return result
         except Exception:
-            return lists
+            return []
