a61e01036dc9c712b37336675b54842d0f1539ad
Cu7ious <328@holbertonschool.com>
2018-05-23 02:13:57 +0000

    Rewrited the load_from_file method + got rid of the encoding

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -86,11 +86,11 @@ class Base:
         Args:
             cls: the reference to the class
         """
-        result = []
         try:
-            with open(cls.__name__ + ".json", "r", encoding="utf-8") as a_file:
+            with open(cls.__name__ + ".json", "r") as a_file:
                 content = cls.from_json_string(a_file.read())
 
+                result = []
                 for el in content:
                     result.append(cls.create(**el))
 
