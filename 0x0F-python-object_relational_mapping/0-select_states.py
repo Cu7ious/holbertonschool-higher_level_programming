0e14f5c74d8affb6cb8fa911ba81948431379680
Cu7ious <328@holbertonschool.com>
2018-07-22 07:17:49 +0000

    Added solution for the task #0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv as argv
+""" Lists all states from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    if len(argv) < 4:
+        print("Error: this script requires 3 arguments")
+        exit()
+
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    connection = MySQLdb.connect(host, user, passwd, db, port)
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM states ORDER BY id')
+    rows = cursor.fetchall()
+    connection.close()
+
+    for row in rows:
+        print(row)
