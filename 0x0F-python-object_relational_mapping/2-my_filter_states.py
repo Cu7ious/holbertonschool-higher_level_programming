6c5e1783ede21dc1694075f4d732898be293f1f2
Cu7ious <328@holbertonschool.com>
2018-07-22 08:05:37 +0000

    Fixed styles

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,7 +2,6 @@
 """ Takes in an argument and displays all values in the `states` table
     of hbtn_0e_0_usa where name matches the argument
 """
-
 if __name__ == "__main__":
     from sys import argv as argv
 
