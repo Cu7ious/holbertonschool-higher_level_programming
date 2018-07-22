91cb5366f629d897eb7aaf341310b41ca802ef73
Cu7ious <328@holbertonschool.com>
2018-07-22 09:19:49 +0000

    Added solution for the task #5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Takes the name of a state as an argument and lists all cities of the state
+    using the database hbtn_0e_4_usa
+"""
+if __name__ == "__main__":
+    from sys import argv as argv
+
+    if len(argv) < 5:
+        print("Error: this script requires 4 arguments")
+        exit()
+
+    import MySQLdb
+
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    state = (argv[4],)
+
+    connection = MySQLdb.connect(host, user, passwd, db, port)
+    cursor = connection.cursor()
+
+    cursor.execute(("SELECT cities.name FROM cities "
+                    "INNER JOIN states ON cities.state_id = states.id "
+                    "WHERE states.name=%s "
+                    "ORDER BY cities.id"), state)
+
+    rows = cursor.fetchall()
+    connection.close()
+
+    print(", ".join(item[0] for item in rows))
