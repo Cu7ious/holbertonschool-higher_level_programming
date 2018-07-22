effd22d2444c706a279106ed986bfb76da7332e5
Cu7ious <328@holbertonschool.com>
2018-07-22 08:58:28 +0000

    Added solution for the task #3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Takes in an argument and displays all values in the `states` table
+    of hbtn_0e_0_usa where name matches the argument (MySQL injections safe)
+"""
+if __name__ == "__main__":
+    from sys import argv as argv
+
+    if len(argv) < 5:
+        print("Error: this script requires 4 arguments")
+        exit()
+
+    import MySQLdb
+
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    name = (argv[4],)
+
+    connection = MySQLdb.connect(host, user, passwd, db, port)
+    cursor = connection.cursor()
+
+    cursor.execute(("SELECT * FROM states "
+                    "WHERE name=%s "
+                    "ORDER BY id"), name)
+
+    rows = cursor.fetchall()
+    connection.close()
+
+    for row in rows:
+        print(row)
