74aaa94e746403e3afde13fe9b27bd92b7b48d09
Cu7ious <328@holbertonschool.com>
2018-07-31 23:39:20 +0000

    Simplified comments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-# This script request the URL provided by user and displays the size
-# of the body of the response in bytes
+# Requests URL provided by user and displays the size of response body in bytes
 curl -Is "$1" | grep "Content-Length:" | cut -d " " -f 2
