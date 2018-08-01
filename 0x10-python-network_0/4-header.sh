1b80cc5182e837033fb8b514097216efb9f4ef39
Cu7ious <328@holbertonschool.com>
2018-08-01 05:47:04 +0000

    Added the -s flag to mute potential errors (task #4)

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes a URL as an argument, sends a GET request to it, displays the body
-curl -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
