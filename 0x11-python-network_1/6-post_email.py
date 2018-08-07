96ebabbe85538cb6c9bc07751b4adae85c7d8eae
Cu7ious <328@holbertonschool.com>
2018-08-07 09:26:33 +0000

    Corrected the styleguide according to PEP8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == "__main__":
-    email = { "email": sys.argv[2] }
+    email = {"email": sys.argv[2]}
     res = requests.post(sys.argv[1], data=email)
 
     print(res.text)
