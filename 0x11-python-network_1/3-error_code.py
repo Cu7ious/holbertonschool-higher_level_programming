b8a07e3c0f776f2ed73376e9b16c3c7c0710de39
Cu7ious <328@holbertonschool.com>
2018-08-07 05:39:41 +0000

    Added solution for the task #3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Get http error codes
+"""
+from urllib import error, request
+from sys import argv
+
+
+if __name__ == "__main__":
+    body = request.Request(argv[1])
+
+    try:
+        with request.urlopen(body) as res:
+            print(res.read().decode("utf-8"))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
