4bee406077da47adfdfb5c4e2d3cf06b2a96d809
Cu7ious <328@holbertonschool.com>
2018-08-07 05:56:03 +0000

    Added solution for the task #7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Prints out the error codes or the response
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    res = requests.get(sys.argv[1])
+
+    if str(res.status_code)[0] == '2':
+        print(res.text)
+    else:
+        print("Error code: {}".format(res.status_code))
