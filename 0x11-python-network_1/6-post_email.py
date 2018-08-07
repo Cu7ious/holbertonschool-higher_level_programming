4ee17ebefc74ab2c4a6fc8733ab93b0d1a90df6d
Cu7ious <328@holbertonschool.com>
2018-08-07 05:55:55 +0000

    Added solution for the task #6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Send data to a site using requests
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    email = { "email": sys.argv[2] }
+    res = requests.post(sys.argv[1], data=email)
+
+    print(res.text)
