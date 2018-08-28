6c20b5149f0188ef7fb7616d7b1791fc8b297da4
Cu7ious <328@holbertonschool.com>
2018-08-28 22:48:29 +0000

    Added solution for the task #1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+ * This script prints 3 lines:
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “Javascript is amazing”
+*/
+console.log("C is fun\nPython is cool\nJavascript is amazing");
