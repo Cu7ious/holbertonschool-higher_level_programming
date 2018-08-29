ae9ff114051f10ea4364895c52c34bfd1ecd20b0
Cu7ious <328@holbertonschool.com>
2018-08-29 05:21:55 +0000

    Added solution for the advanced task #15

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * This module exports function called function that calls function
+ * so you can call function while calling function
+ *
+ * Prototype: function (x, theFunction)
+ * @NB: You are not allowed to use var
+ */
+module.exports = {
+  callMeMoby: function (x, theFunction) {
+    while (x) {
+      theFunction();
+      --x;
+    }
+  }
+};
