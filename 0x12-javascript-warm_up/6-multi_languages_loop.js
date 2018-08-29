166da75df2422dcc8c41320d996e3bed2a23c0f4
Cu7ious <328@holbertonschool.com>
2018-08-29 00:17:27 +0000

    Added solution for the task #6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * This script prints 3 lines by using an array of string and a loop.
+ *
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “Javascript is amazing”
+ * You are not allowed to use any if/else statement
+ * You can use only one console.log
+ * You must use a loop (while, for, etc.)
+ */
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < lines.length; ++i) {
+  console.log(lines[i]);
+}
