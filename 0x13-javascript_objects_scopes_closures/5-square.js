c388c1995f151b1154b064218d089629ad7acb96
Cu7ious <328@holbertonschool.com>
2018-09-05 05:44:37 +0000

    Solution for the task #5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+/**
+ * Rectangle - creates object of type Rectangle
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+class Square extends Rectangle {
+
+  constructor (size) {
+    super(size, size);
+  }
+
+}
+
+module.exports = Square;
