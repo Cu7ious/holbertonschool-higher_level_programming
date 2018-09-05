24b90502127ce01a4e8f886d7317028198afe82d
Cu7ious <328@holbertonschool.com>
2018-09-05 05:43:52 +0000

    Solution for the task #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* Creates the Rectangle class with constructor */
+module.exports = class Rectangle {
+
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+
+};
